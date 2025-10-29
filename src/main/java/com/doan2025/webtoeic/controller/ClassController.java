6ea45a94cabb90223e09e2b7df32ac2c5ad78544
nchuan <nchuan03@gmail.com>
2025-10-29 22:41:06 +0700

    fix theo yeu cau

--- src/main/java/com/doan2025/webtoeic/controller/ClassController.java
+++ src/main/java/com/doan2025/webtoeic/controller/ClassController.java
@@ -88,7 +88,7 @@ public class ClassController {
     }
 
     @PostMapping("/disable-or-delete-notification-in-class")
-    @PreAuthorize("hasRole('TEACHER')")
+    @PreAuthorize("hasRole('TEACHER') or hasRole('MANAGER') or  hasRole('CONSULTANT')")
     public ApiResponse<?> disableOrDeleteNotificationInClass(HttpServletRequest httpServletRequest,
                                                              @RequestBody ClassNotificationRequest dto) {
         return ApiResponse.of(ResponseCode.DELETE_SUCCESS,
@@ -97,7 +97,7 @@ public class ClassController {
     }
 
     @PostMapping("/update-notification-in-class")
-    @PreAuthorize("hasRole('TEACHER')")
+    @PreAuthorize("hasRole('TEACHER') or hasRole('MANAGER') or  hasRole('CONSULTANT')")
     public ApiResponse<?> updateNotificationInClass(HttpServletRequest httpServletRequest,
                                                     @RequestBody ClassNotificationRequest dto) {
         return ApiResponse.of(ResponseCode.UPDATE_SUCCESS,
@@ -106,7 +106,7 @@ public class ClassController {
     }
 
     @PostMapping("/create-notification-in-class")
-    @PreAuthorize("hasRole('TEACHER')")
+    @PreAuthorize("hasRole('TEACHER') or hasRole('MANAGER') or  hasRole('CONSULTANT')")
     public ApiResponse<?> createNotificationInClass(HttpServletRequest httpServletRequest,
                                                     @RequestBody ClassNotificationRequest dto) {
         return ApiResponse.of(ResponseCode.CREATE_SUCCESS,
--- src/main/java/com/doan2025/webtoeic/dto/SearchNotificationInClassDto.java
+++ src/main/java/com/doan2025/webtoeic/dto/SearchNotificationInClassDto.java
@@ -3,6 +3,7 @@ package com.doan2025.webtoeic.dto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class SearchNotificationInClassDto {
@@ -10,4 +11,5 @@ public class SearchNotificationInClassDto {
     private String searchString;
     private Date fromDate;
     private Date toDate;
+    private List<String> notiTypes;
 }
