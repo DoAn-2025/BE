7721d366b4bc6b730446f8a9032cb3b36692136b
nchuan <nchuan03@gmail.com>
2025-12-09 20:09:00 +0700

    final

--- src/main/java/com/doan2025/webtoeic/service/impl/ClassScheduleServiceImpl.java
+++ src/main/java/com/doan2025/webtoeic/service/impl/ClassScheduleServiceImpl.java
@@ -46,7 +46,10 @@ public class ClassScheduleServiceImpl implements ClassScheduleService {
     public Page<?> detailStatisticAttendance(HttpServletRequest httpServletRequest, Long scheduleId, Pageable pageable) {
         User user = userRepository.findByEmail(jwtUtil.getEmailFromToken(httpServletRequest))
                 .orElseThrow(() -> new WebToeicException(ResponseCode.NOT_EXISTED, ResponseObject.USER));
-        if (Objects.equals(user.getRole(), ERole.TEACHER) && classMemberRepository.existsMemberInClass(scheduleId, user.getId())
+        ClassSchedule schedule = classScheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new WebToeicException(ResponseCode.NOT_PERMISSION, ResponseObject.SCHEDULE)
+        );
+        if (Objects.equals(user.getRole(), ERole.TEACHER) && classMemberRepository.existsMemberInClass(schedule.getClazz().getId(), user.getId())
                 || Objects.equals(user.getRole(), ERole.CONSULTANT)
                 || Objects.equals(user.getRole(), ERole.MANAGER)) {
             return attendanceRepository.detailStatisticAttendance(scheduleId, pageable);
